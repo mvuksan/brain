9eaa29addd6732ff0a13a12daa6991b82263f8dc
Milan Vuksan <milan.vuksan@ems.rs>
2020-10-08 13:44:58 +0200

    finish brain

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Nabvar from "./components/Nabvar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start now</h1>
-    </div>
+    <>
+      <Router>
+        <Nabvar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
